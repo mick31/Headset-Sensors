0149c394a64f88fdf1f8bfeb02b57c7b963d1a28
mick31 <mijbennellnl@gmail.com>
2014-02-10 14:06:05 -0800

    tone generator files added

---
+++ Headset Sensors/toneGen.h
@@ -0,0 +1,13 @@
+//
+//  toneGen.h
+//  Headset Sensors
+//
+//  Created by Mick on 2/3/14.
+//  Copyright (c) 2014 Mick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface toneGen : NSObject
+
+@end
---
+++ Headset Sensors/toneGen.m
@@ -0,0 +1,13 @@
+//
+//  toneGen.m
+//  Headset Sensors
+//
+//  Created by Mick on 2/3/14.
+//  Copyright (c) 2014 Mick. All rights reserved.
+//
+
+#import "toneGen.h"
+
+@implementation toneGen
+
+@end
